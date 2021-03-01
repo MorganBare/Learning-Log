9ea253269ee746202171c67da6bb267cc3bbcf82
MorganBare <morganjbare@gmail.com>
2021-03-01 09:06:08 +0100

    Topic() function updated to get all topics and associated entries from the database

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -8,8 +8,9 @@ def index(request):
     # Homepage for learning log
     return render(request, 'learning_logs/index.html')
 
-def topics(request):
-    # Show all topics
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
+def topics(request, topic_id):
+    # Show a single topic and all its entries
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topics.html', context)
